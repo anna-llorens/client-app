cc83f34ca92754ab7d4612fe3c3482ecebc271a6
Anna Llorens <anna.llorens@xing.com>
2023-07-11 16:01:32 +0200

    Remove charts

--- src/components/charts/line-chart.tsx
+++ src/components/charts/line-chart.tsx
@@ -1,6 +1,5 @@
 const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
 
-import { LineChart } from "@mui/x-charts/LineChart";
 import { FC } from "react";
 const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
 const xLabels = [
@@ -14,17 +13,5 @@ const xLabels = [
 ];
 
 export const LineChartStatus: FC = () => {
-  return (
-  
-    <LineChart
-      xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-      series={[
-        {
-          data: [2, 5.5, 2, 8.5, 1.5, 5],
-        },
-      ]}
-      width={500}
-      height={300}
-    />
-  );
+  return <></>;
 };
