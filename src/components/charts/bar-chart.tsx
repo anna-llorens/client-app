1ea6ca89fd4a1b5308225ca87abb616f801c1c71
symphony-annaLlorens <annallorens_12@hotmail.com>
2023-08-12 16:23:07 +0200

    Fixed yarn build

--- src/components/charts/bar-chart.tsx
+++ src/components/charts/bar-chart.tsx
@@ -1,23 +1,5 @@
 import { FC } from "react";
-import { BarChart } from "@mui/x-charts/BarChart";
 
 export const BaBarChartStatus: FC = () => {
-  return (
-    <BarChart
-      xAxis={[
-        {
-          id: "barCategories",
-          data: ["New", "Active", "Completed"],
-          scaleType: "band",
-        },
-      ]}
-      series={[
-        {
-          data: [100, 300, 700],
-        },
-      ]}
-      width={500}
-      height={300}
-    />
-  );
+  return <>Bar chart here some day</>;
 };
